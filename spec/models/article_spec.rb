8455cc4872f95514afe08723e73fcd71fc54cec7
karolina <karolina.frostare@gmail.com>
2020-03-19 11:15:24 +0100

    creates articles model, controller and tests. writes spec for model

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe 'Articles has db columns' do
+    it {is_expected.to have_db_column :id}
+    it {is_expected.to have_db_column :title}
+    it {is_expected.to have_db_column :lead}
+    it {is_expected.to have_db_column :content}
+  end
+
+  describe 'Validations' do
+    it {is_expected.to validate_presence_of :title}
+    it {is_expected.to validate_presence_of :lead}
+    it {is_expected.to validate_presence_of :content}
+  end
+  
+  describe 'Factory' do
+    it 'Should have valid factory' do
+      expect(create(:article)).to be_valid
+    end
+  end
+end
