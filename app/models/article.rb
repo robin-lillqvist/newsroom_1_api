3f3e66d67aa13921dd54e745381a40c00310cf69
emtalen <59577366+emtalen@users.noreply.github.com>
2020-03-21 11:23:09 +0100

    adds attribute category as an enum to articles, modifies model spec

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-  validates_presence_of :title, :lead, :content
+  validates_presence_of :title, :lead, :content, :category
+  enum category: [:latest_news, :tech, :food, :sports, :culture]
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe Article, type: :model do
     it {is_expected.to have_db_column :title}
     it {is_expected.to have_db_column :lead}
     it {is_expected.to have_db_column :content}
+    it {is_expected.to have_db_column :category}
   end
 
   describe 'Validations' do
     it {is_expected.to validate_presence_of :title}
     it {is_expected.to validate_presence_of :lead}
     it {is_expected.to validate_presence_of :content}
+    it {is_expected.to validate_presence_of :category}
   end
   
   describe 'Factory' do
