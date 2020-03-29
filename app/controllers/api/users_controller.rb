936beff2d06e19ae1f844026d449bdb023220d32
kaylawoodbury <kaylasandoval92@gmail.com>
2020-03-29 16:49:54 +0200

    remove unnecessary user controller and serializer

--- app/controllers/api/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::UsersController < ApplicationController
-  before_action :authenticate_user!
-  def show
-    user = User.find(params[:id])
-    render json: user, serializer: UserShowSerializer
-  end
-end
--- app/serializers/user_show_serializer.rb
+++
@@ -1,3 +0,0 @@
-class UserShowSerializer < ActiveModel::Serializer
-  attributes :id, :email, :premium_user
-end
