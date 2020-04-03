d53bc94e07b6687a56aca3e49298be6aa79a1e5d
kaylawoodbury <kaylasandoval92@gmail.com>
2020-04-03 13:49:20 +0200

    update user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe User, type: :model do
 
   describe "Validations" do
     it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :role }
     it { is_expected.to validate_confirmation_of :password }
-    it { is_expected.to validate_confirmation_of :role }
 
     context "should not have an invalid email address" do
       emails = ["asdf@ ds.com", "@example.com", "test me @yahoo.com",
